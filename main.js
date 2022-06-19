a1bee3798b4beb22a0e2f77b2824db085a0de422
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-06-19 14:21:10 +0600

    box demention

--- main.js
+++ main.js
@@ -18,17 +18,37 @@ var viwportHeight = 0;
 setViewPort();
 
 
-function setViewPort() {
-     viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-     viwportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-     console.log(viewportWidth,viwportHeight);
+function setViewPort() {
+    viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    viwportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+    console.log(viewportWidth, viwportHeight);
 }
 
-window.addEventListener('resize', function(event){
-   setViewPort();
+
+
+function getBox() {
+    let box = document.querySelector('.box');
+    return {
+        top: window.scrollY + box.getBoundingClientRect().top,
+        left: window.scrollX + box.getBoundingClientRect().left,
+        width: box.clientWidth,
+        height: box.clientHeight
+    }
+
+}
+
+// dynamic view port on resize
+window.addEventListener('resize', function (event) {
+    setViewPort();
 });
 
+// run the anumation
+requestAnimationFrame(animateBox);
+
 function animateBox() {
+
+    console.log(getBox())
 
 }
