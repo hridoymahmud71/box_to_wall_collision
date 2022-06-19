13f71a05d5e26522f2c6ab26afdb5c30006e90f3
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-06-19 13:50:08 +0600

    dynamic view port on resize

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 // main js here
+
+// check if collision happened in any axis
+var xCollision = false;
+var yCollision = false;
+
+// initial box position
+var xPosition = 0;
+var yPosition = 50;
+
+//  shift in x and y
+var xShift = 10;
+var yShift = 10;
+
+var viewportWidth = 0;
+var viwportHeight = 0;
+
+setViewPort();
+
+
+function setViewPort() {
+     viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+     viwportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+     console.log(viewportWidth,viwportHeight);
+}
+
+window.addEventListener('resize', function(event){
+   setViewPort();
+});
+
+function animateBox() {
+
+}
